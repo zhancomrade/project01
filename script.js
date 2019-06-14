576bdd08d1e60fa1c36b5421323fddc0c3d2af76
Sasha <zhancomrade@gmail.com>
2019-06-14 23:54:30 +0300

    333

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let money = +prompt("Ваш бюджет на месяц?", ""),
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+start();
+
+
 
 let appData = {
         budget: money,
@@ -7,8 +18,28 @@ let appData = {
         expenses: {},
         optionalExpenses: {},
         income: [], 
-        savings: false
+        savings: true
     };
+
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++){
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+    
+        if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+            && a != '' && b != '' && a.length < 50) {
+            
+            console.log("eee");
+    
+            appData.expenses[a] = b;
+        } else {
+            i--;
+        }
+    }
+}
+
+chooseExpenses();
+
 
 // let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
 //     a2 = prompt("Во сколько обойдется?", ''),
@@ -18,24 +49,8 @@ let appData = {
 // appData.expenses.a1 = a2;
 // appData.expenses.a3 = a4;
 
-for (let i = 0; i < 2; i++){
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется?", '');
 
-    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-        && a != '' && b != '' && a.length < 50) {
-        
-        console.log("eee");
-
-        appData.expenses[a] = b;
-    } else {
-        i--;
-    }
-
-    
-};
-
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
@@ -49,3 +64,15 @@ if (appData.moneyPerDay < 100) {
 } else {
     console.log("Err");
 }
+
+function checkSaviings() {
+    if (appData.savings == true) {
+        var save = +prompt("Какова сумма накопления?"),
+            percent = prompt("Под какой процент?");
+        
+            appData.mounthIncome = save/100/12*percent;
+            alert("In mounth: " + appData.mounthIncome);
+    }        
+}
+
+checkSaviings();
