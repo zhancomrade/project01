6308421a6682c088fa0b3105b861d94980573f4c
anzhukov_comrade <anzhukov@diasoft.ru>
2019-06-17 13:41:54 +0300

    444

--- script.js
+++ script.js
@@ -26,8 +26,7 @@ function chooseExpenses() {
         let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
             b = prompt("Во сколько обойдется?", '');
     
-        if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-            && a != '' && b != '' && a.length < 50) {
+        if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
             
             console.log("eee");
     
@@ -50,20 +49,32 @@ chooseExpenses();
 // appData.expenses.a3 = a4;
 
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
 
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log("Min ur");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Mid ur");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Max ur");
-} else {
-    console.log("Err");
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
 }
+
+detectDayBudget();
+
+
+
+// alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+function detectLevel() {
+    
+    if (appData.moneyPerDay < 100) {
+        console.log("Min ur");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Mid ur");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Max ur");
+    } else {
+        console.log("Err");
+    }
+}
+
+detectLevel();
 
 function checkSaviings() {
     if (appData.savings == true) {
